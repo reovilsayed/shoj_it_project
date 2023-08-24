7ed3f2a849bbe494aa2497286ba0aea9b5d1fb50
SazzadFs <sazzadssd512@gmail.com>
2023-08-24 09:06:20 +0600

    slove_project_some issue

--- src/utilitis/SignUpModal/SignUpModal.jsx
+++ src/utilitis/SignUpModal/SignUpModal.jsx
@@ -6,16 +6,16 @@ import { RxCross2 } from "react-icons/rx";
 
 export default function SignUpModal({ setOpen, setSubModal }) {
   return (
-    <div className=" cursor-pointer">
+    <div className=" cursor-pointer relative">
       <RxCross2
         onClick={() => {
           setOpen(false);
         }}
         color="#D6D6D6"
         size="30px"
-        className="cross_btn text-center w-full 2xl:mt-[50px] mt-[16px] hover:rotate-180 duration-500"
+        className="cross_btn hover:rotate-180 duration-500 absolute left-1/2 -translate-x-1/2 top-5"
       />
-      <div className="signUp_container 2xl:px-[141px] 2xl:pt-[72px] xl:pt-2 lg:pt-2 pt-3 xl:px-[132px] lg:px-[110px] px-[70px]">
+      <div className="signUp_container 2xl:px-[141px] 2xl:pt-[72px] xl:pt-[60px] lg:pt-[10px] pt-3 xl:px-[132px] lg:px-[110px] px-[70px] ">
         <div>
           <h3 className=" text-[16px] font-semibold text-[#000] 2xl:pb-[14px] xl:pb-3 pb-3 ">
             Connectez-vous à votre compte
@@ -28,7 +28,7 @@ export default function SignUpModal({ setOpen, setSubModal }) {
             <FaFacebook
               size="28px"
               color="#1877F2"
-              className=" absolute left-2 top-[11px]"
+              className=" absolute left-2 top-[12px]"
             />
             <button className=" w-full py-[14px] ring ring-[#CFCFCF] rounded-full px-2 text-center">
               Continuer avec Facebook
@@ -40,7 +40,7 @@ export default function SignUpModal({ setOpen, setSubModal }) {
             <FcGoogle
               size="28px"
               color="#1877F2"
-              className=" absolute left-2 top-[11px]"
+              className=" absolute left-2 top-[12px]"
             />
             <button className=" w-full py-[14px] ring ring-[#CFCFCF] rounded-full px-2 text-center">
               Continuer avec Google
@@ -48,7 +48,7 @@ export default function SignUpModal({ setOpen, setSubModal }) {
           </div>
           {/* google */}
           {/* divider */}
-          <div className="2xl:pt-[43px] pt-4 2xl:pb-[50px]  pb-4">
+          <div className="2xl:pt-[43px] pt-4 2xl:pb-[43px]  pb-4">
             <Divider>
               <Chip label="Ou bien" />
             </Divider>
