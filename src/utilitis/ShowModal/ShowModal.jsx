d92e99fe3350b6d5f5a7873b385e3900fefa9b75
SazzadFs <sazzadssd512@gmail.com>
2023-08-27 19:14:50 +0600

    finished_project

--- src/utilitis/ShowModal/ShowModal.jsx
+++ src/utilitis/ShowModal/ShowModal.jsx
@@ -14,7 +14,7 @@ export default function ShowModal({ open, setOpen, setSubModal }) {
           aria-labelledby="modal-modal-title"
           aria-describedby="modal-modal-description"
         >
-          <div className=" absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 2xl:w-[654px] xl:w-[654px] lg:w-[654px]  md:w-[654px] w-[90%]  2xl:h-[751px] xl:h-[650px] lg:h-[600px] md:h-auto   bg-[#FFF] rounded-[16px]">
+          <div className=" absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 2xl:w-[654px] xl:w-[654px] lg:w-[654px]  md:w-[654px] w-[90%]  2xl:h-[751px] xl:h-[650px] lg:h-[600px] md:h-auto   bg-[#ffffff] rounded-[16px]">
             <SignUpModal
               setOpen={setOpen}
               open={open}
